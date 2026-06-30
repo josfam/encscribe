e64f966e304e3fb188d177e87f733db1278acea2
josfam <jamukun@proton.me>
2026-06-30 10:46:23 +0300

    Add button style adjustment

--- frontend/src/routes/__root.tsx
+++ frontend/src/routes/__root.tsx
@@ -9,9 +9,9 @@ export const Route = createRootRoute({
 function RootComponent() {
   return (
     <>
-      <div className="bg-gray-500 gap-8 p-4 text-white text-2xl font-bold flex items-center justify-center">
-        <p>Joseph's vault</p>
-        <button className="bg-amber-300 active:bg-amber-500 active:scale-98 hover:bg-amber-400 flex items-center justify-center py-2 px-5 cursor-pointer rounded-full text-gray-900">
+      <div className="bg-gray-200 border-b border-b-gray-400 gap-8 p-4 text-white text-2xl font-bold flex items-center justify-center">
+        <p className="text-gray-950">Joseph's vault</p>
+        <button className="font-normal bg-amber-300 active:bg-amber-500 active:scale-98 hover:bg-amber-400 flex items-center justify-center py-2 border border-amber-500 px-5 cursor-pointer rounded-full shadow-lg text-gray-900">
           Add note
         </button>
       </div>
