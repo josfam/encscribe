49f4cf90de5b93a51dfd7d0a95bbee5ade01b238
josfam <jamukun@proton.me>
2026-06-30 13:34:34 +0300

    Adjust UI for a better look, add logo, favicon and tab name
    - The UI has been polished (colors and buttons)
    - An svg logo has been added
    - Manifest file was changed to be project-specific

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -4,9 +4,9 @@ export const Route = createFileRoute('/')({ component: Home })
 
 function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen">
+    <div className="flex flex-col items-center justify-center min-h-screen gap-8">
       <h1 className="text-4xl text-center font-semibold">You have no notes</h1>
-      <button className="bg-amber-300 active:bg-amber-500 active:scale-98 hover:bg-amber-400 text-2xl font-bold flex items-center justify-center py-2 px-5 cursor-pointer rounded-full text-gray-900">
+      <button className="bg-amber-300 active:bg-amber-500 active:scale-98 hover:bg-amber-400 text-2xl flex items-center justify-center py-2 px-5 cursor-pointer font-normal rounded-full border border-amber-500 shadow-lg text-gray-900">
         Add note
       </button>
     </div>
